6b5ef3dc68be1b2ec5fa81b0eed5a9c37ec1ef81
Heisner, Paul <Paul.Heisner@adesso.de>
2023-10-08 13:55:56 +0200

    bubble, insertion , select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,141 @@
-import Image from 'next/image'
+"use client";
+import ArrayDisplay from "@components/array/ArrayDisplay";
+import { useState } from "react";
+
+const sleep = async (ms: number) =>
+  await new Promise((res) => setTimeout(res, ms));
 
 export default function Home() {
+  const shuffleArray = (array: Array<number>) => {
+    return array
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+  };
+
+  const [array, setArray] = useState(() =>
+    shuffleArray(Array.from({ length: 70 }, (_x, i) => i + 1))
+  );
+  const [horizontal, setHorizontal] = useState(true);
+  const [swapPair, setSwapPair] = useState<[number, number] | null>(null);
+
+  const algos = {
+    selectionSort: async () => {
+      for (let i = 0; i < array.length - 1; i++) {
+        let minIdx = i;
+        for (let j = minIdx; j < array.length; j++) {
+          if (array[minIdx] > array[j]) {
+            minIdx = j;
+          }
+        }
+        if (i != minIdx) {
+          const temp = array[i];
+          array[i] = array[minIdx];
+          array[minIdx] = temp;
+          setSwapPair([i, minIdx]);
+          setArray([...array]);
+          await sleep(20);
+        }
+        setSwapPair(null);
+      }
+    },
+    insertionSort: async () => {
+      for (let index = 1; index < array.length; index++) {
+        const einzusortierenderWert = array[index];
+        let j = index;
+        //suche stellen wo einzusortieren
+        while (j > 0 && einzusortierenderWert < array[j - 1]) {
+          const temp = array[j];
+          array[j] = array[j - 1];
+          array[j - 1] = temp;
+          setSwapPair([j, j - 1]);
+          setArray([...array]);
+          await sleep(5);
+
+          j--;
+        }
+
+        setSwapPair(null);
+        await sleep(20);
+        array[j] = einzusortierenderWert;
+        setArray([...array]);
+        await sleep(20);
+      }
+
+      setSwapPair(null);
+    },
+    bubbleSort: async () => {
+      let swapped = true;
+      for (let index = array.length; swapped; index--) {
+        swapped = false;
+        for (let j = 0; j < index - 1; j++) {
+          if (array[j] > array[j + 1]) {
+            swapped = true;
+            const temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+            setArray([...array]);
+            setSwapPair([j, j + 1]);
+            await sleep(5);
+          }
+        }
+        setSwapPair(null);
+      }
+    },
+  };
+  const [selectedAlgorithm, setselectedAlgorithm] =
+    useState<keyof typeof algos>("insertionSort");
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col items-center gap-4 p-16">
+      <div className="flex items-center p-4">
+        <h1 className="text-xl text-sky-500">Sort Algo Visualization</h1>
+      </div>
+
+      <div className="flex flex-col md:flex-row">
+        <div className="flex">
+          <p>Anordnung der Balken:</p>
+          <input
+            type="radio"
+            id="horizontal"
+            name="horizontal"
+            value="horizontal"
+            checked={horizontal}
+            onChange={() => setHorizontal(true)}
+          />
+            <label htmlFor="horizontal">Horizontal</label>
+          <input
+            type="radio"
+            id="vertical"
+            name="vertical"
+            value="vertical"
+            checked={!horizontal}
+            onChange={() => setHorizontal(false)}
+          />
+            <label htmlFor="vertical">Vertikal</label>
+          <button onClick={() => setArray(shuffleArray(array))}>Shuffle</button>
+          <button onClick={() => algos[selectedAlgorithm]()}>Sort </button>
+          <select
+            value={selectedAlgorithm}
+            onChange={(e) =>
+              setselectedAlgorithm(e.target.value as keyof typeof algos)
+            }
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {Object.keys(algos).map((algo) => (
+              <option key={algo} value={algo}>
+                {algo}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <ArrayDisplay
+            swapPair={swapPair}
+            horizontal={horizontal}
+            value={array}
+          ></ArrayDisplay>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
